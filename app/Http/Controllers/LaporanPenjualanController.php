91136805a05ff820154b681024855ed0f1ba5001
mulyonost <mulyono.tunardy@gmail.com>
2022-01-19 15:21:00 +0800

    Fix

--- app/Http/Controllers/LaporanPenjualanController.php
+++ app/Http/Controllers/LaporanPenjualanController.php
@@ -9,18 +9,17 @@ class LaporanPenjualanController extends Controller
 {
     public function index()
     {
-
-        return view('reports.penjualan.penjualan_index');
+        $penjualan = PenjualanDetail::with('master.customer')->with('aluminium')->get();
+            return view('reports.penjualan.penjualan_index', compact('penjualan'));
+        return view('reports.penjualan.penjualan_index',compact ('penjualan'));
     }
 
     public function search(Request $request)
     {
-        // dd($request);
 
         if ($request->group == "none") {
 
             $penjualan = PenjualanDetail::with('master.customer')->with('aluminium')->get();
-            // dd($penjualan[0]);
             return view('reports.penjualan.penjualan_index', compact('penjualan'));
         } else if ($request->group == "item") {
             echo "This is item group";
